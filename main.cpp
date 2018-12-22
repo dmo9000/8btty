fb8ff4265c26ba86c3aa9c9d8a470ae4ab2c2644
Dan Morriss <daniel.morriss@visionstream.com.au>
2018-12-23 09:29:02 +1100

    Somewhat handle disconnection of shell via EIO.

--- main.cpp
+++ main.cpp
@@ -17,24 +17,24 @@ pthread_t idle_thread;
 
 void *idle_thread_routine(void *)
 {
-		while (1) {
-			//printf("* IDLE TICK *\n");
-			sleep(1);
-			}
+    while (1) {
+        //printf("* IDLE TICK *\n");
+        sleep(1);
+    }
 }
 
 char *envp[] =
 {
-		 "LANG=utf-8", 
-     "HOME=/home/dan",
-     "PATH=/bin:/usr/bin",
-     "TZ=UTC0",
-     "USER=beelzebub",
-     "LOGNAME=tarzan",
-		 "TERM=ansi",
-		 "PS1=[\\u@\\h \\W]\\$ ",
-     0
-    };
+    "LANG=C",
+    "HOME=/home/dan",
+    "PATH=/bin:/usr/bin",
+    "TZ=UTC0",
+    "USER=beelzebub",
+    "LOGNAME=tarzan",
+    "TERM=ansi",
+    "PS1=[\\u@\\h \\W]\\$ ",
+    0
+};
 
 
 int main(int argc, char *argv[])
@@ -52,7 +52,7 @@ int main(int argc, char *argv[])
 
     sleep(1);
 
-		pthread_create( &idle_thread, NULL, idle_thread_routine, NULL);
+    pthread_create( &idle_thread, NULL, idle_thread_routine, NULL);
 
     int masterFd;
     char* args[] = {"/bin/bash", "-i", NULL };
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
     if( procId == 0 ) {
         setenv("TERM", "ansi", 1);
         //myTTY->puts("(in child process...)\n");
-     //   fprintf(stderr, "(in child process)\r\n");
+        //   fprintf(stderr, "(in child process)\r\n");
         execve( args[0], args, envp);
         /* TODO error handling if that didn't work */
     }
@@ -77,14 +77,14 @@ int main(int argc, char *argv[])
 
     ssize_t rd = 0;
     int i = 0;
-		int j = 0;
-		bool ran_input = false;
-		bool ran_output = false;
-		bool asleep = false;
+    int j = 0;
+    bool ran_input = false;
+    bool ran_output = false;
+    bool asleep = false;
 
     while (running) {
-				ran_input = false;
-				ran_output = false;
+        ran_input = false;
+        ran_output = false;
         /* main loop */
         //printf("loop ...\n");
         memset(&in_buffer, 0, 2048);
@@ -94,40 +94,46 @@ int main(int argc, char *argv[])
             for (i = 0; i < rd; i++) {
                 myTTY->putc(in_buffer[i]);
             }
-						ran_input = true;
+            ran_input = true;
+        } else {
+						/* also might get "resource temporarily unavailable" here */
+						if (errno == EIO) {
+		            perror("pty_read");
+								running = false;
+								}
         }
 
-				//printf("buflen = %u\n", j);
-				j = tty_getbuflen();
+        //printf("buflen = %u\n", j);
+        j = tty_getbuflen();
         if (j) {
             c = myTTY->getchar();
-						//printf("c = %u\n", c);
+            //printf("c = %u\n", c);
             if(c != 0) {
                 out_buffer[1] = '\0';
                 out_buffer[0] = c;
                 write(masterFd, &out_buffer, 1);
-								ran_input = true;
+                ran_input = true;
             } else {
                 /* no input waiting */
             }
         }
-			if (!ran_input && !ran_output) {
-					if (!asleep) {
-					//	printf("going to sleep!\n");
-						asleep = true;
-						}
-				
-					} else {
-					if (asleep) {
-						//printf("waking up...\n");
-						asleep = false;
-						}
-					}
+        if (!ran_input && !ran_output) {
+            if (!asleep) {
+                //printf("going to sleep!\n");
+                asleep = true;
+            }
 
-				if (asleep) {
-						usleep(10000);
-						pthread_yield();
-						}
+        } else {
+            if (asleep) {
+                //printf("waking up...\n");
+                asleep = false;
+            }
+        }
+
+        if (asleep) {
+            usleep(10000);
+            pthread_yield();
+        }
     }
 
     exit(0);
